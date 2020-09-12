9716cddc8869ba7d419f75e08898b07b1ef0574c
132yse <1533540012@qq.com>
2020-09-12 19:56:32 +0800

    fix batch-update

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,15 +1,5 @@
 import { scheduleWork, isFn, getCurrentFiber } from './reconciler'
-import {
-  DependencyList,
-  Reducer,
-  IFiber,
-  Dispatch,
-  SetStateAction,
-  EffectCallback,
-  HookTpes,
-  RefObject,
-  IEffect
-} from './type'
+import { DependencyList, Reducer, IFiber, Dispatch, SetStateAction, EffectCallback, HookTpes, RefObject, IEffect } from './type'
 let cursor = 0
 
 export const resetCursor = () => {
@@ -20,10 +10,7 @@ export const useState = <T>(initState: T): [T, Dispatch<SetStateAction<T>>] => {
   return useReducer(null, initState)
 }
 
-export const useReducer = <S, A, Dependency = any>(
-  reducer?: Reducer<S, A>,
-  initState?: S
-): [S, Dispatch<A>] => {
+export const useReducer = <S, A, Dependency = any>(reducer?: Reducer<S, A>, initState?: S): [S, Dispatch<A>] => {
   const [hook, current]: [[S | A, Dependency], IFiber] = getHook<S>(cursor++)
   const setter = (value: A | Dispatch<A>) => {
     let newValue = reducer
@@ -32,9 +19,11 @@ export const useReducer = <S, A, Dependency = any>(
       ? (value as Dispatch<A>)((hook as [A, Dependency])[0])
       : value
     if (newValue !== hook[0]) {
-      current.lane = true
       ;(hook as [S | A | Dispatch<A> | void, Dependency])[0] = newValue
-      scheduleWork()
+      if (!current.lane) {
+        current.lane = true
+        scheduleWork()
+      }
     }
   }
 
@@ -54,11 +43,7 @@ export const useLayout = (cb: EffectCallback, deps?: DependencyList): void => {
   return effectImpl(cb, deps!, 'layout')
 }
 
-const effectImpl = (
-  cb: EffectCallback,
-  deps: DependencyList,
-  key: HookTpes
-): void => {
+const effectImpl = (cb: EffectCallback, deps: DependencyList, key: HookTpes): void => {
   let [hook, current] = getHook(cursor++)
   if (isChanged(hook[1], deps)) {
     hook[0] = useCallback(cb, deps)
@@ -76,10 +61,7 @@ export const useMemo = <S = Function>(cb: () => S, deps?: DependencyList): S => 
   return hook[0]
 }
 
-export const useCallback = <T extends (...args: any[]) => void>(
-  cb: T,
-  deps?: DependencyList
-): T => {
+export const useCallback = <T extends (...args: any[]) => void>(cb: T, deps?: DependencyList): T => {
   return useMemo(() => cb, deps)
 }
 
@@ -87,12 +69,9 @@ export const useRef = <T>(current: T): RefObject<T> => {
   return useMemo(() => ({ current }), [])
 }
 
-export const getHook = <S = Function | undefined, Dependency = any>(
-  cursor: number
-): [[S, Dependency], IFiber] => {
+export const getHook = <S = Function | undefined, Dependency = any>(cursor: number): [[S, Dependency], IFiber] => {
   const current: IFiber<any> = getCurrentFiber()
-  let hooks =
-    current.hooks || (current.hooks = { list: [], effect: [], layout: [] })
+  let hooks = current.hooks || (current.hooks = { list: [], effect: [], layout: [] })
   if (cursor >= hooks.list.length) {
     hooks.list.push([] as IEffect)
   }
